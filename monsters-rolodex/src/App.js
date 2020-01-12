bcdadeca00d2f22ffe39cdea72bcfb8c650957dc
Barbara Rekowska <basnow28@gmail.com>
2020-01-12 18:52:05 +0100

    adding files for github pages

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -2,35 +2,45 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import {CardList} from './components/card-list/card-list.component'
+import {SearchBox} from './components/search-box/search-box.component'
+
 class App extends Component {
   constructor(){
     super()
     this.state = {
-      monsters : [
-        {
-          name: 'Frankenstein',
-          id: 1
-        },
-        {
-          name: 'Dracula',
-          id:2
-        },
-        {
-          name:'Zombie',
-          id:3
-        }
-      ]
+      monsters: [],
+      searchField: ''
     }
   }
 
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+          .then(response => response.json())
+          .then(users => this.setState({monsters:users}))
+  }
+
+  handleChange = text => {
+    this.setState({searchField:text.target.value})
+  }
+
     render(){
+      const {monsters, searchField} = this.state;
+      const filteredMonsters = monsters.filter(monster => 
+        monster.name.toLowerCase().includes(searchField.toLowerCase())  
+      )
+
       return (
         <div className="App">
-          {this.state.monsters.map(monster => (
-            <h1 key ={monster.id}>{monster.name}</h1>
-          ))}
+          <h1>Monsters Rolodex</h1>
+          <SearchBox 
+            placeholder = 'search monsters' 
+            handleChange = {this.handleChange}
+            />
+          <CardList monsters = {filteredMonsters}/>
         </div>
   );
+
 }
 }
 
---
+++ monsters-rolodex/src/components/search-box/search-box.component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './search-box.styles.css';
+
+export const SearchBox = props => {
+    return (
+    <input 
+        type='search' 
+        className='search'
+        placeholder={props.placeholder}
+        onChange = {props.handleChange}
+    />)
+}

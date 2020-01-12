5e1bfba2a8c18382686a01171806c6aed8ab89f6
Barbara Rekowska <basnow28@gmail.com>
2020-01-12 18:51:24 +0100

    Updates

---
+++ precache-manifest.d52b17a91f07d1e5eb41589615e0acac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3cda9ab0ca0bcdb1462d6c4be324deb",
+    "url": "/udemyReactCourse-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cfda040b9da14322219d",
+    "url": "/udemyReactCourse-monsters-rolodex/static/css/main.bbca0162.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/udemyReactCourse-monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/udemyReactCourse-monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cfda040b9da14322219d",
+    "url": "/udemyReactCourse-monsters-rolodex/static/js/main.1cca978f.chunk.js"
+  },
+  {
+    "revision": "09746e110d6dc68f53cc",
+    "url": "/udemyReactCourse-monsters-rolodex/static/js/runtime-main.536d3bfd.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/udemyReactCourse-monsters-rolodex/static/media/logo.ee7cd8ed.svg"
+  }
+]);
